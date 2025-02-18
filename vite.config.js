f9112f8a48231e71042a2eed34b808ff1915fcb8
yunyayaya <yunwork200727@gmail.com>
2025-02-18 15:18:58 +0800

    gh-page建立

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.Node_ENV === 'production' ? '/week2_hw/' : '/',
   plugins: [react()],
 })
